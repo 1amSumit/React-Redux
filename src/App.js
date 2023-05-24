542f0bfecabea018a22b4223dff5406c5dd87a31
1amSumit <sj779619@gmail.com>
2023-05-24 19:15:51 +0530

    Login and Logout implemented using Redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import Counter from './components/Counter';
+// import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
 
-
-function App() {
+const App = () => {
+  const isLogedIn = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <>
+      <Header />
+      {!isLogedIn && <Auth />}
+      {isLogedIn && <UserProfile />}
+      {/* <Counter /> */}
+    </>
   );
-}
+};
 
 export default App;
